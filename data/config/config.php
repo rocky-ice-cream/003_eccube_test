c85b0d605a85c06fa18b43351112b7407b41b4e3
rock-tack <snowwave1224@gmail.com>
2015-09-14 23:19:28 +0900

    first commit

---
+++ data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('HTTP_URL', 'http://localhost/~rocky/eccube/html/');
+define('HTTPS_URL', 'http://localhost/~rocky/eccube/html/');
+define('ROOT_URLPATH', '/~rocky/eccube/html/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'eccube_test_user');
+define('DB_PASSWORD', 'eccube_test_pass');
+define('DB_SERVER', '127.0.0.1');
+define('DB_NAME', 'eccube_test');
+define('DB_PORT', '');
+define('ADMIN_DIR', 'admin/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'driogiphouphainajoutouresleweawesipruniv');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'mail');
+define('SMTP_HOST', '');
+define('SMTP_PORT', '');
+define('SMTP_USER', '');
+define('SMTP_PASSWORD', '');
